3eb9e0a59e5631d3b70ad89fa4d56aad6f35baa7
MaxxyDaWolf <whalemo71@gmail.com>
2024-06-06 14:17:28 -0400

    Update urun.js

--- urun.js
+++ urun.js
@@ -2,7 +2,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = "//inglan2.github.io/uRun/popup.html"),
+		(e.src = "https://raw.githubusercontent.com/TheGooferFox/testing12312312/main/popup.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "uRun",
